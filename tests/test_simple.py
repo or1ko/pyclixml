a161f87afd7461b8476fd51011a3ffad415e39d6
kouji <kouji1985@hotmail.com>
2019-02-14 20:59:51 +0900

    string <S>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,7 +1,20 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
+import pyclixml as cli
+import xml.etree.ElementTree as ET
+import unittest
 
+class TestCliXmlParser(unittest.TestCase):
+    def test_string(self):
+        exampleXml = """
+        <S xmlns="http://schemas.microsoft.com/powershell/2004/04">This is String</S>
+        """
 
-def test_success():
-    assert True
+        parser = ET.XMLParser(target=cli.CliXMLParser())
+        parser.feed(exampleXml)
+        ret = parser.close()
+        self.assertEqual("This is String", ret)
+
+if __name__ == "__main__":
+    unittest.main()
